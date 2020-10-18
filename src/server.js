b4a371853aef5ff69f38b8739bf74aafaf4e331c
Jamilly Vitorya da Silva Barbosa <jamillyvitorya14@gmail.com>
2020-10-18 10:49:46 -0300

    back-end test_0.1

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 //importar biblioteca
 const express = require('express');
 const path = require('path');
+
 //iniciando o express
 const server = express();
+server
+
+
+//utilizando os arquivos estaticos
+.use(express.static('public'))
+
 
 //criar rota
-server.get('/', (request, response) => {
+.get('/', (request, response) => {
+    // console.log(request.query)
     return response.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
+
 
 //iniciar servidor
 server.listen(5500)
