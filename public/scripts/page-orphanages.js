ecbaf36e02ad0d725d1ea5b08b316633471c2648
Jamilly Vitorya da Silva Barbosa <jamillyvitorya14@gmail.com>
2020-10-18 13:05:19 -0300

    back-end meninas e meninos

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,30 +1,45 @@
 // criar mapa
-const map = L.map('mapid').setView([-27.222633/*latitude */, -49.6455874/*longitude*/ ],/*zoom*/ 15)
+const map = L.map("mapid").setView(
+  [-27.222633 /*latitude */, -49.6455874 /*longitude*/],
+  /*zoom*/ 15
+)
 
-L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
 
 // criar icon
 const icon = L.icon({
-    iconUrl: './img/map-marker.svg',
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],/*onde estará ancorado*/
-    popupAnchor: [170, 2]
-}
-)
+  iconUrl: "./img/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68] /*onde estará ancorado*/,
+  popupAnchor: [170, 2],
+})
 
- //criar popup overlay
-const popup = L.popup({
+
+//na function a gente agrupa logicas
+function addMarker({id, name, lat, lng}) {    
+
+  //criar popup overlay
+  const popup = L.popup({
     closeButton: false,
-    className: 'map-popup',
+    className: "map-popup",
     minWidth: 240,
-    minHeight: 240
+    minHeight: 240,
+  }).setContent(`${name}<a href="/orphanage?id=${id}><img src="/img/arrow-white.svg"></a>`)
+
+  // criar e adicionar marker
+  L
+  .marker([lat, lng], { icon })
+  .addTo(map)
+  .bindPopup(popup)
 }
-).setContent('Lar das Meninas <a href="orphanage?id=1" class="choose-orphanages"> <img src="./img/arrow-white.svg"></a>')
 
-
-// criar e adicionar marker
-L.marker([-27.222633, -49.6455874], { icon })
-.addTo(map)
-.bindPopup(popup)
-
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach(span => {
+    const orphanage ={
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+    addMarker(orphanage)
+})
