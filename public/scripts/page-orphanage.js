e0eaf12a5a47124e6c8c412ad6ed752048fd1cd5
Jamilly Vitorya da Silva Barbosa <jamillyvitorya14@gmail.com>
2020-10-18 23:11:31 -0300

    final

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,52 +1,95 @@
+// const options = {
+//     dragging: false,
+//     touchZoom: false,
+//     doubleClickZoom: false,
+//     scrollWheelZoom: false,
+//     zoomControl: false,
+// }
+
+// // criar mapa
+// const map = L.map('mapid').setView([-27.222633/*latitude */, -49.6455874/*longitude*/ ],/*zoom*/ 15)
+
+// L.tileLayer(
+//     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+// // criar icon
+// const icon = L.icon({
+//     iconUrl: './img/map-marker.svg',
+//     iconSize: [58, 68],
+//     iconAnchor: [29, 68],/*onde estará ancorado*/
+//     popupAnchor: [170, 2]
+// }
+// )
+
+// // criar e adicionar marker
+// L.marker([-27.222633, -49.6455874], { icon })
+// .addTo(map)
+
+
+// /* galeria de imagem*/
+
+// function selectImage(event) {
+    
+//     const button = event.currentTarget
+
+//     //remover classes .active
+//     const buttons = document.querySelectorAll(".images button")
+//     buttons.forEach(removeActiveClass)
+
+//     function removeActiveClass(button){
+//         button.classList.remove("active")
+//     }
+    
+//     //selecionar a imagem clicada
+//     const image = button.children[0]
+//     const imageContainer = document.querySelector(".orphanage-details > img")
+
+//     //atualizar o container de imagem
+//     imageContainer.src = image.src
+
+//     //adicionar a class . active para este botao
+//     button.classList.add('active')
+// }
 const options = {
     dragging: false,
     touchZoom: false,
     doubleClickZoom: false,
     scrollWheelZoom: false,
     zoomControl: false,
-}
-
-// criar mapa
-const map = L.map('mapid').setView([-27.222633/*latitude */, -49.6455874/*longitude*/ ],/*zoom*/ 15)
-
-L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
-
-// criar icon
-const icon = L.icon({
-    iconUrl: './img/map-marker.svg',
+  };
+  
+  const lat = document.querySelector("span[data-lat]").dataset.lat;
+  const lng = document.querySelector("span[data-lng]").dataset.lng;
+  
+  const map = L.map("mapid", options).setView([lat, lng], 15);
+  
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+  
+  const icon = L.icon({
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
-    iconAnchor: [29, 68],/*onde estará ancorado*/
-    popupAnchor: [170, 2]
-}
-)
-
-// criar e adicionar marker
-L.marker([-27.222633, -49.6455874], { icon })
-.addTo(map)
-
-
-/* galeria de imagem*/
-
-function selectImage(event) {
-    
-    const button = event.currentTarget
-
-    //remover classes .active
-    const buttons = document.querySelectorAll(".images button")
-    buttons.forEach(removeActiveClass)
-
-    function removeActiveClass(button){
-        button.classList.remove("active")
+    iconAnchor: [29, 68],
+    popAnchor: [170, 2],
+  });
+  
+  L.marker([lat, lng], { icon }).addTo(map);
+  
+  /* image gallery */
+  
+  function selectImage(event) {
+    const button = event.currentTarget;
+  
+    const buttons = document.querySelectorAll(".images button");
+    buttons.forEach(removeActiveClass);
+  
+    function removeActiveClass(button) {
+      button.classList.remove("active");
     }
-    
-    //selecionar a imagem clicada
-    const image = button.children[0]
-    const imageContainer = document.querySelector(".orphanage-details > img")
-
-    //atualizar o container de imagem
-    imageContainer.src = image.src
-
-    //adicionar a class . active para este botao
-    button.classList.add('active')
-}
+  
+    const image = button.children[0];
+    const imageContainer = document.querySelector(".orphanage-details > img");
+  
+    imageContainer.src = image.src;
+  
+    button.classList.add("active");
+  }
--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -5,7 +5,8 @@ module.exports = [
         lat: "-27.222633", 
         lng: "-49.6455874",
         name: "Lar das Meninas",
-        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "9992227700",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
         images: [
             "https://images.unsplash.com/photo-1586022773518-47a6bf08fa90?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
             
@@ -16,11 +17,13 @@ module.exports = [
         open_on_weekends: "1"
     },
 
-    {id: 2,
+    {
+        id: 2,
         lat: "-27.222633", 
         lng: "-49.6555874",
         name: "Lar dos Meninos",
-        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "9992227700",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
         images: [
             "https://images.unsplash.com/photo-1586022773518-47a6bf08fa90?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
             
